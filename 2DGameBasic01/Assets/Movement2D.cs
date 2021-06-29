55fbcb1c206d67282c799a6032ec9ab62dce0f9d
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-06-30 00:07:32 +0900

    designated movement

--- 2DGameBasic01/Assets/Movement2D.cs
+++ 2DGameBasic01/Assets/Movement2D.cs
@@ -1,14 +1,26 @@
-
 using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
+    private float   moveSpeed = 5.0f;               // 이동 속도
+    private Vector3 moveDirection = Vector3.zero;   // 이동 방향
+
     private void Update()
     {
-        // 새로운 위치 = 현재 위치 + (방향 * 속도)
-        //transform.position = transform.position + new Vector3(1, 0, 0) * 1;
+        // Negative lefet, a : -1
+        // Positive right, d : 1
+        // Non : 0
+        float x = Input.GetAxisRaw("Horizontal");   // 좌우 이동
+        // Negative down, s : -1
+        // Positive up, w : 1
+        // Non : 0
+        float y = Input.GetAxisRaw("Vertical");     // 위아래 이동
 
-        transform.position += Vector3.right * 1 * Time.deltaTime;
+        // 이동방향 설정
+        moveDirection = new Vector3(x, y, 0);
+
+        // 새로운 위치 = 현재 위치 + (방향 * 속도)
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 
 }
