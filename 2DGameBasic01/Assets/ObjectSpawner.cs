f1fb31411f53c84c0dc437c28c58463a824bad3e
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-06-30 23:38:47 +0900

    Random prefab with random location

--- 2DGameBasic01/Assets/ObjectSpawner.cs
+++ 2DGameBasic01/Assets/ObjectSpawner.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField]
+    private int objectSpawnCount = 30;
+    [SerializeField]
     private GameObject[] prefabArray;
 
     private void Awake()
     {
-        for (int i = 0; i < 10; i ++)
+        for (int i = 0; i < objectSpawnCount; ++ i)
         {
             int index = Random.Range(0, prefabArray.Length);
-            Vector3 position = new Vector3(-4.5f + i, 0, 0);
+            float x = Random.Range(-7.5f, 7.5f);    // x 위치
+            float y = Random.Range(-4.5f, 4.5f);    // y 위치
+            Vector3 position = new Vector3(x, y, 0);
 
             Instantiate(prefabArray[index], position, Quaternion.identity);
         }
