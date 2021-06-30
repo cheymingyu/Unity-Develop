f0c8d53071e1f1e68c150e0b23d4785085194094
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-01 00:08:06 +0900

    Shoot bullet

--- 2DGameBasic01/Assets/PlayerController.cs
+++ 2DGameBasic01/Assets/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private GameObject bulletPrefab;
     private float moveSpeed = 3.0f;
+    private Vector3 lastMoveDirection = Vector3.right;
 
     private void Update()
     {
@@ -16,6 +17,12 @@ public class PlayerController : MonoBehaviour
 
         transform.position += new Vector3(x, y, 0) * moveSpeed * Time.deltaTime;
 
+        // 마지막에 입력된 방향키의 방향을 총알의 발사 방향으로 활용
+        if (x != 0 || y != 0)
+        {
+            lastMoveDirection = new Vector3(x, y, 0);
+        }
+
         // 플레이어 오브젝트 총알 발사
         if (Input.GetKeyDown(keyCodeFire))
         {
@@ -24,6 +31,8 @@ public class PlayerController : MonoBehaviour
             clone.name = "Bullet";
             clone.transform.localScale = Vector3.one * 0.5f;
             clone.GetComponent<SpriteRenderer>().color = Color.red;
+
+            clone.GetComponent<Movement2D>().Setup(lastMoveDirection);
         }
     }
 }
