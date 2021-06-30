f29745ae51926d915aeb4f13afc873e2cfad8f92
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-06-30 21:18:30 +0900

    object collision

--- 2DGameBasic01/Assets/Movement2D.cs
+++ 2DGameBasic01/Assets/Movement2D.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 public class Movement2D : MonoBehaviour
 {
     private float   moveSpeed = 5.0f;               // 이동 속도
-    private Vector3 moveDirection = Vector3.zero;   // 이동 방향
+    private Rigidbody2D rigid2D;
+
+    private void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
@@ -16,11 +21,15 @@ public class Movement2D : MonoBehaviour
         // Non : 0
         float y = Input.GetAxisRaw("Vertical");     // 위아래 이동
 
-        // 이동방향 설정
-        moveDirection = new Vector3(x, y, 0);
+        //// 이동방향 설정
+        //moveDirection = new Vector3(x, y, 0);
 
-        // 새로운 위치 = 현재 위치 + (방향 * 속도)
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        //// 새로운 위치 = 현재 위치 + (방향 * 속도)
+        //transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        // Rigidbody2D 컴포넌트에 있는 속력(velocity) 변수 설정
+        rigid2D.velocity = new Vector3(x, y, 0) * moveSpeed;
+
     }
 
 }
