aa7ce65f62297f2481d3a26217a98b77741bb806
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-06-30 23:57:48 +0900

    Spawn object in order

--- 2DGameBasic01/Assets/ObjectSpawner.cs
+++ 2DGameBasic01/Assets/ObjectSpawner.cs
@@ -8,10 +8,22 @@ public class ObjectSpawner : MonoBehaviour
     private GameObject[] prefabArray;
     [SerializeField]
     private Transform[] spawnPointArray;
+    private int currentObjectiCount = 0;    // 현재까지 생성한 오브젝트 개수
+    private float objectSpawnTime = 0.0f;
 
-    private void Awake()
+    private void Update()
     {
-        for (int i = 0; i < objectSpawnCount; ++ i)
+        // objectSpawnCount 개수만큼만 생성하고 더 이상 생성하지 않도록 하기 위해 설정
+        if (currentObjectiCount + 1 > objectSpawnCount)
+        {
+            return;
+        }
+
+        // 원하는 시간마다 오브젝트를 생성하기 위한 시간 변수 연산
+        objectSpawnTime += Time.deltaTime;
+
+        // 0.5초에 한번씩 실행
+        if (objectSpawnTime >= 0.5f)
         {
             int prefabIndex = Random.Range(0, prefabArray.Length);
             int spawnIndex = Random.Range(0, spawnPointArray.Length);
@@ -23,6 +35,10 @@ public class ObjectSpawner : MonoBehaviour
             // spawnIndex가 1인 오브젝트가 오른쪽에 있기 때문에 왼쪽으로 이동
             Vector3 moveDirection = (spawnIndex == 0 ? Vector3.right : Vector3.left);
             clone.GetComponent<Movement2D>().Setup(moveDirection);
+
+            currentObjectiCount++;      // 현재 생성된 오브젝트의 개수를 1 증가시킨다.
+            objectSpawnTime = 0.0f;     // 시간을 0으로 초기화 해야 다시 0.5초를 계산할 수 있다.
         }
+        
     }
 }
