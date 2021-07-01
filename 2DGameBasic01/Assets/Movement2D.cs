a7862a94c2780e8435e405b7d20524ec1a039706
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-01 16:04:35 +0900

    Ground jump

--- 2DGameBasic01/Assets/Movement2D.cs
+++ 2DGameBasic01/Assets/Movement2D.cs
@@ -10,13 +10,27 @@ public class Movement2D : MonoBehaviour
     [HideInInspector]
     public bool isLongJump = false;             // 낮은 점프, 높은 점프 체크
 
+    [SerializeField]
+    private LayerMask           groundLayer;        // 바닥 체크를 위한 충돌 레이어
+    private CapsuleCollider2D   capsuleCollider2D;  // 오브젝트의 충돌 범위 컴포넌트
+    private bool                isGrounded;         // 바닥 체크 (바닥에 닿아있을 때 true)
+    private Vector3             footPosition;       // 발의 위치
+
     private void Awake()
     {
         rigid2D = GetComponent<Rigidbody2D>();
+        capsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
 
     private void FixedUpdate()
     {
+        // 플레이어 오브젝트의 Collider2D min, center, max 위치 정보
+        Bounds bounds = capsuleCollider2D.bounds;
+        // 플레이어의 발 위치 설정
+        footPosition = new Vector2(bounds.center.x, bounds.min.y);
+        // 플레이어의 발 위치에 원을 생성하고, 원이 바닥과 닿아있으면 isGrounded = true
+        isGrounded = Physics2D.OverlapCircle(footPosition, 0.1f, groundLayer);
+
         // 낮은 점프, 높은 점프 구현을 위한 중력 계수(gravityScale) 조절 (Jump Up일 때만 적용)
         // 중력 계수가 낮은 if 문은 높은 점프가 되고, 중력 계수가 높은 else 문은 낮은 점프가 된다.
         if (isLongJump && rigid2D.velocity.y > 0)
@@ -29,6 +43,12 @@ public class Movement2D : MonoBehaviour
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(footPosition, 0.1f);
+    }
+
     public void Move(float x)
     {
         // x축 이동은 x * speed로, y축 이동은 기존의 속력 값(현재는 중력)
@@ -37,7 +57,10 @@ public class Movement2D : MonoBehaviour
 
     public void Jump()
     {
-        rigid2D.velocity = Vector2.up * jumpForce;
+        if (isGrounded == true)
+        {
+            rigid2D.velocity = Vector2.up * jumpForce;
+        }
     }
 
 }
