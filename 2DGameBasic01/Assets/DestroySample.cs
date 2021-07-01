daa85b1df910f2467500a0f659ce279bd14fad4d
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2021-07-01 15:02:15 +0900

    Destroy objects

--- 2DGameBasic01/Assets/DestroySample.cs
+++ 2DGameBasic01/Assets/DestroySample.cs
@@ -7,7 +7,13 @@ public class DestroySample : MonoBehaviour
 
     private void Awake()
     {
-        // playerObject 게임오브젝트의 "PlayerController" 컴포넌트 삭제
-        Destroy(playerObject.GetComponent<PlayerController>());
+        //// playerObject 게임오브젝트의 "PlayerController" 컴포넌트 삭제
+        //Destroy(playerObject.GetComponent<PlayerController>());
+
+        // playerObject 게임오브젝트를 삭제
+        //Destroy(playerObject);
+
+        // playerObject 게임오브젝트를 2초 뒤에 삭제
+        Destroy(playerObject, 2.0f);
     }
 }
